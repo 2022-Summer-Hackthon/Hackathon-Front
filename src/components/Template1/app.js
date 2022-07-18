d1fc8fff00e7db388f0d4c224940d6cd581a2414
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-18 09:15:53 +0900

    test

---
+++ src/components/Template1/app.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="front">
+      <h1 className="title">DGSW</h1>
+      <h2 className="title">dgsw.hs.kr</h2>
+    </div>
+    <div className="back">
+      <h1 className="title1">DGSW</h1>
+      <h2 className="title1">dgsw.hs.kr</h2>
+    </div>
+    <div className="contents">
+      <p className="career">Developer</p>
+      <p className="name">강성훈</p>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
