32cc9f58a2199bcfe5fd21501206ccb66cf53302
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-07-19 15:20:58 +0900

    template1

--- src/components/Template1/Template1Front.jsx
+++ src/components/Template1/Template1Front.jsx
@@ -1,16 +1,17 @@
+import { memo } from "react";
 import Template1Front from "./templateFront.style";
 
-function Template1() {
+const Template1 = memo(({ size }) => {
   return (
     <Template1Front>
-      <div className="front">
+      <div className={`${size}-front`}>
         <div>
-          <h1 className="title">DGSW</h1>
-          <h2 className="title">dgsw.hs.kr</h2>
+          <h1 className={`${size}-title1`}>DGSW</h1>
+          <h2 className={`${size}-title2`}>dgsw.hs.kr</h2>
         </div>
       </div>
     </Template1Front>
   );
-}
+});
 
 export default Template1;
