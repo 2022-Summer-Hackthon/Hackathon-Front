ecf46e087692c37a36536c8a75f96a607466cb09
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-07-19 16:29:04 +0900

    template3

--- src/components/Template3/Template3Front.jsx
+++ src/components/Template3/Template3Front.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { memo } from "react";
 import Template3FrontStyle from "./Template3Front.style";
 
-const Template3Front = () => {
+const Template3Front = memo(({ size }) => {
   return (
     <Template3FrontStyle>
-      <div className="large-front">
-        <div className="large-top"></div>
-        <div className="large-bottom"></div>
-        <h1 className="large-title1">DGSW</h1>
-        <h2 className="large-title2">frontend developer</h2>
+      <div className={`${size}-front`}>
+        <div className={`${size}-top`}></div>
+        <div className={`${size}-bottom`}></div>
+        <h1 className={`${size}-title1`}>DGSW</h1>
+        <h2 className={`${size}-title2`}>frontend developer</h2>
       </div>
     </Template3FrontStyle>
   );
-};
+});
 
 export default Template3Front;
