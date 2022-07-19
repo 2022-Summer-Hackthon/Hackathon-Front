0480d6996c573f8e2593155ffd2ca83bfa260ce5
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-07-19 15:32:42 +0900

    template2

--- src/components/Template2/template2Front.style.js
+++ src/components/Template2/template2Front.style.js
@@ -1,9 +1,53 @@
 import styled from "styled-components";
 
 const Template2FrontStyle = styled.div`
-  .front {
+  .nomal-front {
     width: 616px;
     height: 351px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 50px;
+    background-color: white;
+    border: 1px solid black;
+  }
+  .nomal-title {
+    color: black;
+  }
+  .nomal-title1 {
+    width: 431px;
+    height: 110px;
+
+    font-family: "Arial";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 55px;
+    letter-spacing: 0.05em;
+
+    color: #000000;
+  }
+  .nomal-title2 {
+    width: 431px;
+    height: 23px;
+
+    font-family: "Arial";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 23px;
+    /* identical to box height */
+
+    letter-spacing: 0.05em;
+
+    color: #9f9f9f;
+    /* identical to box height */
+  }
+
+  .large-front {
+    width: 1050px;
+    height: 598px;
     display: flex;
     justify-content: center;
     align-items: center;
